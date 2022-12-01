1b9323312e80e7877382a70c32d97f7a6f9db205
Sigolletes <juditreverter87@gmail.com>
2022-12-01 09:53:59 +0100

    Add step2 to courseinfo

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -6,18 +6,36 @@ const Header = (props) => {
   )
 }
 
+const Part1 = (props) => {
+  return (
+    <p>
+      {props.part1} {props.exercises1}
+    </p>
+  )
+}
+
+const Part2 = (props) => {
+  return (
+    <p>
+      {props.part2} {props.exercises2}
+    </p>
+  )
+}
+
+const Part3 = (props) => {
+  return (
+    <p>
+      {props.part3} {props.exercises3}
+    </p>
+  )
+}
+
 const Content = (props) => {
   return (
     <div>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+      <Part1 part1={props.part1} exercises1={props.exercises1} />
+      <Part2 part2={props.part2} exercises2={props.exercises2} />
+      <Part3 part3={props.part3} exercises3={props.exercises3} /> 
     </div>
   )
 }
